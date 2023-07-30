aec8536adb6c67420ec7ebf73d5442dd09816e26
diyana <labibadiyana100@gmail.com>
2023-07-31 04:01:01 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        client.connect();
 
         const usersCollection = client.db("languagevioDB").collection("users");
         const classesCollection = client.db("languagevioDB").collection("classes");
